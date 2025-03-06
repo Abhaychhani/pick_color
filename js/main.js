7ef3c384e92722350e3d955e0ca7c756aeb8622e
Abhay verma <abhaychhani@gmail.com>
2025-03-06 22:15:02 +0530

    nav for mobales fixed

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ class Main {
     constructor(){
         this.navIcon = document.querySelector('.menu-sm-icon');
         this.menu = document.querySelector('.menu-sm');
+        this.closeNavBtn = document.querySelector('.close-nav');
         this.isMenuOpen=false;
 
         this.colors=new Colors();
@@ -11,6 +12,9 @@ class Main {
     }
     #eventListeners(){
         this.navIcon.onclick=(e)=>{this.toggleNav()};
+        this.closeNavBtn.onclick=()=>{
+            this.toggleNav();
+        }
         this.menu.onclick=(e)=>{
             if(e.target.classList.contains('menu-link')){
                 this.toggleNav();
@@ -19,11 +23,9 @@ class Main {
     }
     toggleNav(){
         if(this.isMenuOpen){
-            this.navIcon.innerHTML=`<i class="fa-solid fa-bars"></i>`
             this.menu.setAttribute('style','visibility:hidden; left:100%; opacity:0;')
             this.isMenuOpen=false;
         }else{
-            this.navIcon.innerHTML=`<i class="fa-solid fa-xmark" style="color:white; filter:drop-shadow( 2px 2px 0px #333); font-size:2rem;" ></i>`
             this.menu.setAttribute('style','visibility:visible; left:0%; opacity:1;')
             this.isMenuOpen=true;
         }
